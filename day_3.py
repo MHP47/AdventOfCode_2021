74cbbecf95969ef4492b538218f4a82076e0aa64
Morgan Waldron <morgan.waldron@versent.com.au>
2021-12-03 16:50:44 +1100

    Day 3, cleaned up

--- day_3.py
+++ day_3.py
@@ -5,56 +5,40 @@ from collections import Counter
 def part_1(p_Input):
     x = p_Input.splitlines()
     x = [[a[i] for a in x] for i in range(len(x[0]))]
-    gamma = ''
-    epsilon = ''
+    gamma = epsilon = ''
     for i in range(len(x)):
-        a = x[i].count('0')
-        b = x[i].count('1')
-        gamma += '0' if a > b else '1'
-        epsilon += '0' if a < b else '1'
+        mc = Counter(x[i]).most_common()
+        gamma += mc[0][0]
+        epsilon += mc[1][0]
     return int(gamma,2) * int(epsilon,2)
-
 
 
 def part_2(p_Input):
     x = p_Input.splitlines()
-    cnt = len(x)
-    tmp = x
-    oxygen = co2 = []
+    itm_len = len(x[0])
+    oxygen = co2 = x
 
-
-    y = [[a[i] for a in tmp] for i in range(len(tmp[0]))]
-    for i in range(cnt):
-        if len(tmp) == 1:
+    for i in range(itm_len):
+        if len(oxygen) == len(co2) == 1:
             break
-        mc = Counter(cat(list(zip(y))[i][0])).most_common(2)
-        if mc[0][1] == mc[1][1]:
-            mc = '1'
-        else:
-            mc = mc[0][0]
-        tmp = [z for z in tmp if z[i] == mc]
-        y = [[a[i] for a in tmp] for i in range(len(tmp[0]))]
 
-    oxygen = tmp[0]
-    tmp = x
-    y = [[a[i] for a in tmp] for i in range(len(tmp[0]))]
+        if len(oxygen) > 1:
+            mc = Counter(cat(list(zip([[z[i] for z in oxygen] for i in range(itm_len)]))[i][0])).most_common()
+            if mc[0][1] == mc[1][1]:
+                mc_bit = '1'
+            else:
+                mc_bit = mc[0][0]
+            oxygen = [z for z in oxygen if z[i] == mc_bit]
 
-    for i in range(len(x)):
-        if len(tmp) == 1:
-            break
-        mc = Counter(cat(list(zip(y))[i][0])).most_common()
-        if mc[0][1] == mc[1][1]:
-            mc = '0'
-        else:
-            mc = mc[1][0]
-        tmp = [z for z in tmp if z[i] == mc]
-        y = [[a[i] for a in tmp] for i in range(len(tmp[0]))]
-
-    co2 = tmp[0]
-    return int(oxygen,2) * int(co2,2)
-
-
-
+        if len(co2) > 1:
+            mc = Counter(cat(list(zip([[z[i] for z in co2] for i in range(itm_len)]))[i][0])).most_common()
+            if mc[0][1] == mc[1][1]:
+                mc_bit = '0'
+            else:
+                mc_bit = mc[1][0]
+            co2 = [z for z in co2 if z[i] == mc_bit]
+    
+    return int(oxygen[0],2) * int(co2[0],2)
 
 example_input_1 = '''00100
 11110
