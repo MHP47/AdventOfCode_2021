55676a9c87f76651dee176002e45ef2f4a976ac0
Morgan Waldron <morgan.waldron@versent.com.au>
2021-12-04 17:10:21 +1100

    Day 4, part 2
    Actually missed the req of "has at least one complete row or column" in part 1, so added in the column check
    Got stuck for a while because missed the case of more than 1 board finishing at the same time

--- day_4.py
+++ day_4.py
@@ -17,7 +17,12 @@ def part_1(p_Input):
 
     def check_complete(p_board):
         for b,brd in p_board.items():
+            # Check rows
             for r,nums in brd.items():
+                if all([isinstance(x, bool) for x in nums]):
+                    return b
+            # Check cols
+            for nums in list(zip(*list(brd.values()))):
                 if all([isinstance(x, bool) for x in nums]):
                     return b
         return None
@@ -35,7 +40,50 @@ def part_1(p_Input):
 
 
 def part_2(p_Input):
-    pass
+    numbers = list(map(int, p_Input.splitlines()[0].split(',')))
+    bingo = defaultdict(list)
+    boards_list = defaultdict(dict)
+    boards = p_Input.splitlines()[2:]
+    boards = [boards[x:x+5] for x in range(0, len(boards), 6)]
+    for b in range(len(boards)):
+        l = len(boards[b][0].split())
+        for r in range(l):
+            boards_list[b][r] = []
+            for i,n in enumerate(parse_ints(boards[b][r])):
+                bingo[n].append((b, r, i))
+                boards_list[b][r].append(n)
+
+    boards_list = dict(boards_list)
+
+    def check_complete(p_board):
+        ret = set()
+        for b,brd in p_board.items():
+            # Check rows
+            for r,nums in brd.items():
+                if all([isinstance(x, bool) for x in nums]):
+                    ret.add(b)
+            # Check cols
+            for nums in list(zip(*list(brd.values()))):
+                if all([isinstance(x, bool) for x in nums]):
+                    ret.add(b)
+        return None if not ret else ret
+
+    for n in numbers:
+        for b,r,i in bingo[n]:
+            try:
+                boards_list[b][r][i] = True
+            except KeyError:
+                pass
+        winner = check_complete(boards_list)
+        if isinstance(winner, set):
+            if len(boards_list.values()) == 1:
+                break
+            for i in winner:
+                del boards_list[i]
+    count = 0
+    for _,nums in list(boards_list.values())[0].items():
+        count += sum([x for x in nums if not isinstance(x, bool)])
+    return count * n
 
 
 example_input_1 = '''7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
@@ -63,5 +111,5 @@ challenge_input = Input('4')
 assert(part_1(example_input_1) == 4512)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == 'None')
+assert(part_2(example_input_1) == 1924)
 print(f"Part 2: {part_2(challenge_input)}")
