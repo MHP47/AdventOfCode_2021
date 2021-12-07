e5ebaafa7fd47670a1410b97f76fbd10d6211e4a
Morgan Waldron <morgan.waldron@versent.com.au>
2021-12-07 16:19:03 +1100

    Day 7, part 2

--- day_7.py
+++ day_7.py
@@ -9,7 +9,12 @@ def part_1(p_Input):
 
 
 def part_2(p_Input):
-    pass
+    coords = parse_ints(p_Input)
+    cmin, cmax = min(coords), max(coords)
+    vals = []
+    for dest in range(cmin, cmax):
+        vals.append(sum([(y/2)*(1+y) for y in [abs(dest - x) for x in coords]]))
+    return int(min(vals))
 
 
 example_input_1 = '16,1,2,0,4,2,7,1,2,14'
@@ -18,5 +23,5 @@ challenge_input = Input('7')
 assert(part_1(example_input_1) == 37)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == 'None')
+assert(part_2(example_input_1) == 168)
 print(f"Part 2: {part_2(challenge_input)}")
