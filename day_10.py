cca4da1bbc6fe2591320c6f135f2fbefcc473c32
Morgan Waldron <morgan.waldron@versent.com.au>
2021-12-10 16:35:39 +1100

    Day 10, part 2

--- day_10.py
+++ day_10.py
@@ -3,6 +3,7 @@ from utils import *
 OPENS = '({[<'
 CLOSES = ')}]>'
 SCORES = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
+SCORES_2 = { ')': 1, ']': 2, '}': 3, '>': 4 }
 
 def part_1(p_Input):
     score = 0
@@ -19,7 +20,22 @@ def part_1(p_Input):
     return score
 
 def part_2(p_Input):
-    pass
+    data = p_Input.splitlines()
+    scores = []
+    for i, r in enumerate(data):
+        p = ''
+        while p != r:
+            p = r
+            for c in (r'\[\]', r'\(\)', '<>', r'{}'):
+                r = re.sub(c, '', r)
+        if all([x in OPENS for x in r]):
+            score = 0
+            for x in r[::-1]:
+                score *= 5
+                score += SCORES_2[CLOSES[OPENS.index(x)]]
+            scores.append(score)
+    return sorted(scores)[(len(scores)-1)//2]
+
 
 
 example_input_1 = '''[({(<(())[]>[[{[]{<()<>>
@@ -38,5 +54,5 @@ challenge_input = Input('10')
 assert(part_1(example_input_1) == 26397)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == 'None')
+assert(part_2(example_input_1) == 288957)
 print(f"Part 2: {part_2(challenge_input)}")
