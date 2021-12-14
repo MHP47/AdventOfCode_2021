bde73ec89b232907e572dd405a1bb7f459663b34
Morgan Waldron <morgan.waldron@versent.com.au>
2021-12-14 21:27:28 +1100

    Day 13, part 2

--- day_13.py
+++ day_13.py
@@ -20,8 +20,22 @@ def part_1(p_Input):
     return len(dots.keys())
 
 def part_2(p_Input):
-    pass
+    dots, folds = p_Input.split('\n\n')
+    dots = defaultdict(lambda: ' ', {tuple(map(int,x.split(','))): '#' for x in dots.splitlines()})
+    
+    for f in folds.splitlines():
+        if 'y=' in f:
+            line = parse_ints(f)[0]
+            for old,new in [((x,y),(x,line-y+line)) for x,y in dots.keys() if y > line]:
+                del dots[old]
+                dots[new] = '#'
+        else:
+            line = parse_ints(f)[0]
+            for old,new in [((x,y),(line-x+line,y)) for x,y in dots.keys() if x > line]:
+                del dots[old]
+                dots[new] = '#'
 
+    return '\n'.join([cat([dots[(x,y)] for x in range((max(dots.keys(), key=lambda x: x[0]))[0]+1)]) for y in range(max(dots.keys(), key=lambda x: x[1])[1]+1)])
 
 example_input_1 = '''6,10
 0,14
@@ -50,5 +64,5 @@ challenge_input = Input('13')
 assert(part_1(example_input_1) == 17)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == 'None')
-print(f"Part 2: {part_2(challenge_input)}")
+print("Part 2:")
+print(part_2(challenge_input))
