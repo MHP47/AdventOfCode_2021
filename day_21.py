029024728209214748e73301f6b0624aa639a8b7
Morgan Waldron <morgan.waldron@versent.com.au>
2021-12-21 17:54:59 +1100

    Day 21, part 2

--- day_21.py
+++ day_21.py
@@ -1,3 +1,5 @@
+import functools
+from itertools import count
 from utils import *
 
 
@@ -26,7 +28,21 @@ def part_1(p_Input):
 
 
 def part_2(p_Input):
-    pass
+    players = dict(tuple(parse_ints(x)) for x in p_Input.splitlines())
+    rolls = {3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
+
+    @functools.lru_cache(maxsize=None)
+    def turn(player1, player2, player1_score=0, player2_score=0):
+        if player2_score >= 21:
+            return 0, 1
+        p1_wins, p2_wins = 0, 0
+        for k,v in rolls.items():
+            loc = (player1 + k) % 10 or 10
+            p2_wins_x, p1_wins_x = turn(player2, loc, player2_score, player1_score+loc)
+            p1_wins, p2_wins = p1_wins + p1_wins_x*v, p2_wins + p2_wins_x*v
+        return p1_wins, p2_wins
+
+    return max(turn(*tuple(players.values())))
 
 
 example_input_1 = '''Player 1 starting position: 4
@@ -37,5 +53,6 @@ challenge_input = Input('21')
 assert(part_1(example_input_1) == 739785)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == 'None')
+assert(part_2(example_input_1) == 444356092776315)
+assert(part_2(challenge_input) == 309196008717909)
 print(f"Part 2: {part_2(challenge_input)}")
