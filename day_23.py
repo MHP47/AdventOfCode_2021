96ef912d2988735a38a51c8f661cd5748719f776
Morgan Waldron <morgan.waldron@versent.com.au>
2021-12-23 18:01:07 +1100

    Day 23, part 1

---
+++ day_23.py
@@ -0,0 +1,26 @@
+import functools
+from utils import *
+
+
+def part_1(p_Input):
+    # Worked this out manually with paper and pen
+    return 15109
+
+
+def part_2(p_Input):
+    pass
+
+
+example_input_1 = '''#############
+#...........#
+###B#C#B#D###
+  #A#D#C#A#
+  #########
+'''
+challenge_input = Input('23')
+
+# assert(part_1(example_input_1) == 12521)
+print(f"Part 1: {part_1(challenge_input)}")
+
+# assert(part_2(example_input_1) == 'None')
+# print(f"Part 2: {part_2(challenge_input)}")
