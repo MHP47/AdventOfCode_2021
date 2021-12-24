51cd671c18179a18c445da7fc38c16562956002d
Morgan Waldron <morgan.waldron@versent.com.au>
2021-12-25 00:28:50 +1100

    Day 24, part 2

--- day_24.py
+++ day_24.py
@@ -1,139 +1,6 @@
 from utils import *
 
 
-class ALU():
-    def __init__(self, commands) -> None:
-        self.vars = {'w': 0, 'x': 0, 'y': 0, 'z': 0}
-        self.commands_list = [tuple(x.split(' ')) for x in commands.splitlines()]
-
-    def run(self, number):
-        nums = deque(number)
-        for c, *o in self.commands_list:
-            if c == 'inp':
-                self.inp(*o, nums.popleft())
-            else:
-                getattr(self, c)(*o)
-
-    def inp(self, a, b):
-        self.vars[a] = b
-
-    def add(self, a, b):
-        try:
-            tmp = int(b)
-        except ValueError:
-            tmp = self.vars[b]
-            
-        self.vars[a] += tmp
-
-    def mul(self, a, b):
-        try:
-            tmp = int(b)
-        except ValueError:
-            tmp = self.vars[b]
-
-        self.vars[a] *= tmp
-
-    def div(self, a, b):
-        try:
-            tmp = int(b)
-        except ValueError:
-            tmp = self.vars[b]
-
-        self.vars[a] //= tmp
-
-    def mod(self, a, b):
-        try:
-            tmp = int(b)
-        except ValueError:
-            tmp = self.vars[b]
-
-        self.vars[a] %= tmp
-
-    def eql(self, a, b):
-        try:
-            tmp = int(b)
-        except ValueError:
-            tmp = self.vars[b]
-
-        self.vars[a] = 1 if self.vars[a] == tmp else 0
-
-    def display(self):
-        print(f'W: {self.vars["w"]} X: {self.vars["x"]} Y: {self.vars["y"]} Z: {self.vars["z"]} ')
-        # pprint(list(enumerate(self.commands_list,1)))
-        # return f'W: {self.vars["w"]}\nX: {self.vars["x"]}\nY: {self.vars["y"]}\nZ: {self.vars["z"]}\n'
-
-    def get(self):
-        if self.vars['z'] == 0:
-            return True
-        return False
-    
-    def proc(self, number):
-        assert(9999999999999 < number <= 99999999999999)
-        nums = deque(map(int, list(str(number))))
-
-        n1 = nums.popleft()
-        self.vars['z'] = n1 + 1
-
-        n2 = nums.popleft()
-        self.vars['z'] *= 26
-        self.vars['z'] += n2 + 7
-
-        n3 = nums.popleft()
-        self.vars['z'] *= 26
-        self.vars['z'] += n3 + 13
-
-        n4 = nums.popleft()
-        x = 0 if (self.vars['z']%26)-6 == n4 else 1
-        self.vars['z'] //= 26
-        self.vars['z'] = (self.vars['z']*(25+x)+n4+10) if x == 1 else self.vars['z']
-
-        n5 = nums.popleft()
-        self.vars['z'] *= 26
-        self.vars['z'] += n5
-
-        n6 = nums.popleft()
-        x = 0 if (self.vars['z']%26)-4 == n6 else 1
-        self.vars['z'] //= 26
-        self.vars['z'] = (self.vars['z']*(25+x)+n6+13) if x == 1 else self.vars['z']
-
-        n7 = nums.popleft()
-        self.vars['z'] *= 26
-        self.vars['z'] += n7 + 11
-
-        n8 = nums.popleft()
-        self.vars['z'] *= 26
-        self.vars['z'] += n8 + 6
-
-        n9 = nums.popleft()
-        self.vars['z'] *= 26
-        self.vars['z'] += n9 + 1
-
-        n10 = nums.popleft()
-        x = 0 if (self.vars['z']%26)-0 == n10 else 1
-        self.vars['z'] //= 26
-        self.vars['z'] = (self.vars['z']*(25+x)+n10+7) if x == 1 else self.vars['z']
-
-        n11 = nums.popleft()
-        x = 0 if (self.vars['z']%26)-0 == n11 else 1
-        self.vars['z'] //= 26
-        self.vars['z'] = (self.vars['z']*(25+x)+n11+11) if x == 1 else self.vars['z']
-
-        n12 = nums.popleft()
-        x = 0 if (self.vars['z']%26)-3 == n12 else 1
-        self.vars['z'] //= 26
-        self.vars['z'] = (self.vars['z']*(25+x)+n12+14) if x == 1 else self.vars['z']
-
-        n13 = nums.popleft()
-        x = 0 if (self.vars['z']%26)-9 == n13 else 1
-        self.vars['z'] //= 26
-        self.vars['z'] = (self.vars['z']*(25+x)+n13+4) if x == 1 else self.vars['z']
-
-        n14 = nums.popleft()
-        x = 0 if (self.vars['z']%26)-9 == n14 else 1
-        self.vars['z'] //= 26
-        self.vars['z'] = (self.vars['z']*(25+x)+n14+10) if x == 1 else self.vars['z']
-
-
 def part_1(p_Input):
     commands_list = p_Input.split('inp w\n')[1:]
     # The only lines that differ between each group are 3, 4 and 14
@@ -154,7 +21,23 @@ def part_1(p_Input):
     return cat(map(str, model))
 
 def part_2(p_Input):
-    pass
+    commands_list = p_Input.split('inp w\n')[1:]
+    # The only lines that differ between each group are 3, 4 and 14
+    z = []
+    model = [0] * 14
+    for i,c in enumerate(commands_list):
+        cmds = c.splitlines()
+        x_add = int(cmds[4].split()[-1])
+        y_add = int(cmds[14].split()[-1])
+        if cmds[3] == 'div z 1':
+            z.append((i, y_add))
+        elif cmds[3] == 'div z 26':
+            j, tmp = z.pop()
+            tmp += x_add
+            model[i] = 1 + tmp if tmp > 0 else 1
+            model[j] = 1 - tmp if tmp < 0 else 1
+
+    return cat(map(str, model))
 
 
 example_input_1 = '''inp x
